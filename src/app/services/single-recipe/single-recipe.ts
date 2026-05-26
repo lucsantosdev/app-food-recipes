be5c488d3000bfd802409507dc8c4ee81f4a1f54
Lucas Santos <lucsantosdev@gmail.com>
2026-05-26 11:48:11 -0300

    fix: single recipe page loading state

--- src/app/services/single-recipe/single-recipe.ts
+++ src/app/services/single-recipe/single-recipe.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { timeout } from 'rxjs';
 import { environment } from '../../../environments/environments'
 
 @Injectable({
@@ -11,11 +12,11 @@ export class SingleRecipe {
 
   takeRecipe(id: number) {
     const apiUrl = `${environment.apiBaseUrl}?endpoint=recipe&id=${id}`;
-    return this.http.get(apiUrl);
+    return this.http.get(apiUrl).pipe(timeout(15000));
   }
 
   takeSimilarRecipes(id: number) {
     const apiUrl = `${environment.apiBaseUrl}?endpoint=similar&id=${id}`;
-    return this.http.get(apiUrl);
+    return this.http.get(apiUrl).pipe(timeout(15000));
   }
 }
